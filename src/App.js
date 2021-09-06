83b1240ec9db499db2b95b6830aa0ebb93c44d5a
Michael <michael@graphicdesire.se>
2021-09-06 13:04:11 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="header">
+				<div className="title">The Recipe Cave</div>
+				<div>
+					<nav>
+						<ul className="desktop-navigation">
+							<li>Home</li>
+							<li>Recipes</li>
+							<li>Contact</li>
+						</ul>
+						<ul className="mobile-navigation">
+							<li>Home</li>
+							<li>Recipes</li>
+							<li>Contact</li>
+						</ul>
+					</nav>
+				</div>
+			</header>
+		</div>
+	);
 }
 
 export default App;
