c5e3326dea01f424e003c384acfc67406cdff1c0
ddewe <ddewee92@gmail.com>
2021-09-13 14:32:14 +0200

    Documentation for Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,10 +13,13 @@ class Footer extends Component {
         this.setState({ email: e.target.value });
     };
 
+    /**
+     * Only lets you submit for a newsletter if the email provided is valid. If yes, alerts you that you successfully subscribed.
+     */
     submitHandler = (e) => {
         e.preventDefault();
         alert(
-            "Thank you for subscribing to our eminent newsletter, you will be spammed for all eternity. Enjoy!"
+            "Successfully subscribed. Thank you for subscribing to our eminent newsletter, you will be spammed for all eternity. Enjoy!"
         );
     };
 
