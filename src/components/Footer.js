ebe1e89fd0ef0e0919aa38dda4d215e65559aefb
ddewe <ddewee92@gmail.com>
2021-09-13 13:48:15 +0200

    changed Footer to class component, added newsletter validation

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,45 +1,80 @@
-import React from "react";
-import { Form } from "react-bootstrap";
+import React, { Component } from "react";
+import { Form, Button } from "react-bootstrap";
 
-function Footer() {
-	return (
-		<footer className="footer">
-			<div className="footer-socials">
-				<div className="footer-socials text">Be social!</div>
-				<ul className="social-links">
-					<li>
-						<a href="#!">
-							<i className="fab fa-facebook-f"></i>
-						</a>
-					</li>
-					<li>
-						<a href="#!">
-							<i className="fab fa-twitter"></i>
-						</a>
-					</li>
-					<li>
-						<a href="#!">
-							<i className="fab fa-instagram"></i>
-						</a>
-					</li>
-				</ul>
-			</div>
-			<div className="newsletter">
-				<form method="post" action="#!">
-					<Form.Label>Subscribe to our newsletter!</Form.Label>
-					<Form.Control type="email" placeholder="name@example.com" />
-				</form>
-			</div>
-			<div className="footer-nav">
-				<li>
-					<a href="#!">About us</a>
-				</li>
-				<li>
-					<a href="#!">Jobs</a>
-				</li>
-			</div>
-		</footer>
-	);
+class Footer extends Component {
+    constructor() {
+        super();
+        this.state = {
+            email: "",
+        };
+    }
+
+    changeHandler = (e) => {
+        this.setState({ email: e.target.value });
+    };
+
+    submitHandler = (e) => {
+        e.preventDefault();
+        alert(
+            "Thank you for subscribing to our eminent newsletter, you will be spammed for all eternity. Enjoy!"
+        );
+    };
+
+    render() {
+        return (
+            <footer className="footer">
+                <div className="footer-socials">
+                    <div className="footer-socials text">Be social!</div>
+                    <ul className="social-links">
+                        <li>
+                            <a href="#!">
+                                <i className="fab fa-facebook-f"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#!">
+                                <i className="fab fa-twitter"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#!">
+                                <i className="fab fa-instagram"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="newsletter">
+                    <form
+                        htmlFor="newsletter"
+                        onSubmit={(e) => this.submitHandler(e)}
+                    >
+                        <Form.Label>Subscribe to our newsletter!</Form.Label>
+                        <Form.Control
+                            type="email"
+                            onChange={this.changeHandler}
+                            placeholder="name@example.com"
+                        />
+                        <Button
+                            variant="outline-secondary"
+                            htmlFor="newsletter"
+                            type="submit"
+                            className="newsletter-btn"
+                        >
+                            Subscribe!
+                        </Button>
+                    </form>
+                </div>
+                <div className="footer-nav">
+                    <li>
+                        <a href="#!">About us</a>
+                    </li>
+                    <li>
+                        <a href="#!">Jobs</a>
+                    </li>
+                </div>
+            </footer>
+        );
+    }
 }
 
 export default Footer;
--- src/components/ShowSingleRecipe.js
+++ src/components/ShowSingleRecipe.js
@@ -12,74 +12,91 @@ import { Rating } from "@material-ui/lab";
  */
 
 export default function ShowSingleRecipe() {
-	let { recipeId } = useParams();
-	const [recipeData, setRecipeData] = useState([]);
-	const [ingredient, setIngredients] = useState([]);
-	const [rating, setRating] = useState(3);
-	const history = useHistory();
+    let { recipeId } = useParams();
+    const [recipeData, setRecipeData] = useState([]);
+    const [ingredient, setIngredients] = useState([]);
+    const [rating, setRating] = useState(3);
+    const history = useHistory();
 
-	useEffect(() => {
-		Axios.get(`http://localhost:3002/api/getFromId/${recipeId}`).then((data) => {
-			setRecipeData(data.data);
-		});
-	}, []); // eslint-disable-line react-hooks/exhaustive-deps
+    useEffect(() => {
+        Axios.get(`http://localhost:3002/api/getFromId/${recipeId}`).then(
+            (data) => {
+                setRecipeData(data.data);
+            }
+        );
+    }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-	useEffect(() => {
-		Axios.get(`http://localhost:3002/api/getIngredientsFromId/${recipeId}`).then((data) => {
-			setIngredients(data.data);
-		});
-	}, []); // eslint-disable-line react-hooks/exhaustive-deps
+    useEffect(() => {
+        Axios.get(
+            `http://localhost:3002/api/getIngredientsFromId/${recipeId}`
+        ).then((data) => {
+            setIngredients(data.data);
+        });
+    }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-	// converts minutes to hours and minutes
-	function convertTime(min) {
-		let time = min;
-		let hours = Math.floor(time / 60);
-		let minutes = time % 60;
-		return hours + " hrs " + minutes;
-	}
+    // converts minutes to hours and minutes
+    function convertTime(min) {
+        let time = min;
+        let hours = Math.floor(time / 60);
+        let minutes = time % 60;
+        return hours + " hrs " + minutes;
+    }
 
-	return recipeData.map((recipe, key) => {
-		return (
-			<div key={key} className="header zindex">
-				<br />
-				<a className="link wipe" href="#!" onClick={() => history.goBack()}>
-					<ArrowLeftCircleFill size="15" /> BACK
-				</a>
-				<div className="stars-bar">
-					<Rating
-						name="simple-controlled"
-						value={rating}
-						onChange={(e, newValue) => {
-							setRating(newValue);
-						}}
-					/>
-				</div>
-				<div className="single-recipe">
-					<img src={`/images/recipe/${recipeId}.jpg`} alt={recipe.title} className="large-image" />
-					<h3 className="title">{recipe.title}</h3>
-					<p>
-						<Clock size="16" />
-						<span>
-							&nbsp;Preparation: {recipe.prep_time} minutes | Cooking: {recipe.cooking_time >= 60 ? convertTime(recipe.cooking_time) : recipe.cooking_time} minutes
-						</span>
-					</p>
-					<p>{recipe.description}</p>
-					<h4 className="title">You'll need:</h4>
-					<ul className="ingredient-list">
-						{ingredient.map((ingr, ingkey) => {
-							return (
-								<li key={ingkey}>
-									{ingr.measure} {ingr.ingredient}
-								</li>
-							);
-						})}
-					</ul>
-					<h4 className="title">Step by step:</h4>
-					<ul className="ingredient-list numbered">
-						<RecipeSteps recipeId={recipeId} />
-					</ul>
-				</div>
-			</div>
-		);
-	});
+    return recipeData.map((recipe, key) => {
+        return (
+            <div key={key} className="header zindex">
+                <br />
+                <a
+                    className="link wipe"
+                    href="#!"
+                    onClick={() => history.goBack()}
+                >
+                    <ArrowLeftCircleFill size="15" /> BACK
+                </a>
+                <div className="stars-bar">
+                    <Rating
+                        name="simple-controlled"
+                        value={rating}
+                        onChange={(e, newValue) => {
+                            setRating(newValue);
+                        }}
+                    />
+                </div>
+                <div className="single-recipe">
+                    <img
+                        src={`/images/recipe/${recipeId}.jpg`}
+                        alt={recipe.title}
+                        className="large-image"
+                    />
+                    <h3 className="title">{recipe.title}</h3>
+                    <p>
+                        <Clock size="16" />
+                        <span>
+                            &nbsp;Preparation: {recipe.prep_time} minutes |
+                            Cooking:{" "}
+                            {recipe.cooking_time >= 60
+                                ? convertTime(recipe.cooking_time)
+                                : recipe.cooking_time}{" "}
+                            minutes
+                        </span>
+                    </p>
+                    <p>{recipe.description}</p>
+                    <h4 className="title">You'll need:</h4>
+                    <ul className="ingredient-list">
+                        {ingredient.map((ingr, ingkey) => {
+                            return (
+                                <li key={ingkey}>
+                                    {ingr.measure} {ingr.ingredient}
+                                </li>
+                            );
+                        })}
+                    </ul>
+                    <h4 className="title">Step by step:</h4>
+                    <ul className="ingredient-list numbered">
+                        <RecipeSteps recipeId={recipeId} />
+                    </ul>
+                </div>
+            </div>
+        );
+    });
 }
