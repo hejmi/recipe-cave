a5dfb7f69ea33a33ff44bd0d8a39c864dc3ca554
ddewe <ddewee92@gmail.com>
2021-09-20 11:24:06 +0200

    Send MASSAGE fixed

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
+import { sendForm } from "emailjs-com";
 
 function Contact() {
+    const [toSend, setToSend] = useState({
+        fromname: "",
+        message: "",
+        replyto: "",
+    });
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        sendForm(
+            "service_bpskx3i",
+            "template_37kp03i",
+            "#contactForm",
+            "user_VySAGNSZd2vNt2qb3jvXm"
+        ).then(
+            (result) => {
+                console.log(result.text);
+                alert("Thank you for reaching out");
+                setToSend({
+                    fromname: "",
+                    message: "",
+                    replyto: "",
+                });
+            },
+            (error) => {
+                console.log(error.text);
+            }
+        );
+    };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setToSend({ ...toSend, [name]: value });
+    };
+
     return (
         <div className="contact-main">
             <div className="contact-title">
@@ -9,13 +45,19 @@ function Contact() {
             </div>
 
             <div className="contact-forms">
-                <Form>
+                <Form onSubmit={sendEmail} id="contactForm">
                     <Form.Group
                         className="mb-3"
                         controlId="exampleForm.ControlInput1"
                     >
                         <Form.Label>Name</Form.Label>
-                        <Form.Control type="text" placeholder="Your name" />
+                        <Form.Control
+                            type="text"
+                            placeholder="Your name"
+                            name="fromname"
+                            value={toSend.fromname}
+                            onChange={handleChange}
+                        />
                     </Form.Group>
                     <Form.Group
                         className="mb-3"
@@ -25,6 +67,9 @@ function Contact() {
                         <Form.Control
                             type="email"
                             placeholder="name@example.com"
+                            name="replyto"
+                            value={toSend.replyto}
+                            onChange={handleChange}
                         />
                     </Form.Group>
                     <Form.Group
@@ -35,13 +80,17 @@ function Contact() {
                         <Form.Control
                             as="textarea"
                             placeholder="Write something!"
+                            name="message"
                             rows={2}
+                            value={toSend.message}
+                            onChange={handleChange}
                         />
                     </Form.Group>
                     <Button
                         variant="outline-secondary"
                         htmlFor="message"
                         type="submit"
+                        onSubmit={sendEmail}
                     >
                         Send Massage
                     </Button>
